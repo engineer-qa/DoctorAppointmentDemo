2736ff95a19633e81e2b79c3cba94c6d23d69e2e
Vyacheslav <vyacheslav.popov.qa@gmail.com>
2025-11-13 18:08:52 +0200

    Initial commit

---
+++ DoctorAppointmentDemo.Data/Configuration/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace MyDoctorAppointment.Data.Configuration
+{
+    public static class Constants
+    {
+        // заменить на путь валидный для вашей директории на пк (в будущем будем использовать относительный путь)
+        public const string AppSettingsPath = "D:\\Programs\\Fork\\DoctorAppointmentDemo\\DoctorAppointmentDemo.Data\\Configuration\\appsettings.json";
+    }
+}
